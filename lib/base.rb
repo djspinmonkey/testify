4e64edfec274e7bae551abf142b9bb64c6d08779
John Hyland <github@djspinmonkey.com>
2009-11-03 15:24:46 -0500

    generalize Base.framework a bit

--- lib/base.rb
+++ lib/base.rb
@@ -2,9 +2,10 @@ require 'result_set'
 
 module Testify
   class Base
-    def self.framework (fw)
+    def self.framework (fw = nil)
       class_eval do
-        @@framework = Testify::Frameworks::find(fw)
+        @@framework = Testify::Frameworks::find(fw) if fw
+        @@framework
       end
     end
 
