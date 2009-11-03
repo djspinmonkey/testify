e2c20372dc2ed158b6fdef400684358f09852cd9
John Hyland <github@djspinmonkey.com>
2009-11-03 18:38:04 -0500

    add destroy_class method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,10 @@ require 'spec/autorun'
 Spec::Runner.configure do |config|
   
 end
+
+# Doesn't work for anything inside a module (eg, if you try to remove
+# Testify::Frameworks::RSpec it will totally blow up).
+def destroy_class ( klass )
+  klass = klass.name.to_s if klass.kind_of? Class
+  Object.class_exec { remove_const klass } if Object.const_defined? klass
+end
