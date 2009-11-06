ecf6e562a1fc0e18c8c680bd10d4a3d43b1b45b4
John Hyland <github@djspinmonkey.com>
2009-11-06 16:51:42 -0500

    add more common statuses

---
+++ lib/status/failed.rb
@@ -0,0 +1,6 @@
+module Testify
+  module Status
+    class Failed < Testify::Status::Base
+    end
+  end
+end
---
+++ lib/status/pending.rb
@@ -0,0 +1,6 @@
+module Testify
+  module Status
+    class Pending < Testify::Status::Base
+    end
+  end
+end
