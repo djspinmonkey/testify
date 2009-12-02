b7e17a1bf62cf1cae9311ba456a07899146a8836
John Hyland <github@djspinmonkey.com>
2009-12-02 15:55:00 -0500

    cleanup and refactoring

--- lib/framework/base.rb
+++ lib/framework/base.rb
@@ -4,6 +4,8 @@ module Testify
       Base.subclasses
     end
 
+    DEFAULT_STATUSES = [ :passed, :pending, :failed, :error ].collect { |status| Testify::Status::Base.find(status) }
+
     ##
     # The class from which all Framework classes must inherit.  Keeps track of
     # all known frameworks and any aliases they're known by.
@@ -12,21 +14,18 @@ module Testify
       extend Testify::Aliasable
       extend Testify::SubclassAware
 
-      DEFAULT_STATUSES = [ :passed, :pending, :failed, :error ].collect { |status| Testify::Status::Base.find(status) }
-
+      # Annoying alias chain required to preserve inerited() methods from modules
       class << self; alias :old_inherited :inherited end
       def self.inherited(sub)
         old_inherited(sub)
-        sub.class_eval { @@statuses = DEFAULT_STATUSES.dup }
+        sub.class_eval { @statuses = Testify::Framework::DEFAULT_STATUSES.dup }
       end
 
-      def self.statuses(*stats)
-        class_eval do
-          if stats.any?
-            @@statuses = stats.collect { |stat| @@statuses.push Testify::Status::Base.find(stat) }
-          end
-          @@statuses || default_statuses
+      def self.statuses(*statuses)
+        if statuses.any?
+          @statuses = statuses.collect { |status| Testify::Status::Base.find(status) }
         end
+        @statuses
       end
     end
   end
