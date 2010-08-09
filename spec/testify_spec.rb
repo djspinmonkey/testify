fe6adf16ca5bd534c3b272671893faba4cc31651
John Hyland <jhyland@damballa.com>
2010-08-08 20:06:31 -0400

    de-cheesify the .version test

--- spec/testify_spec.rb
+++ spec/testify_spec.rb
@@ -1,26 +1,26 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 
-describe "The Testify module" do
+describe "Testify" do
 
-  it "should provide an env hash with some default values" do
-    defaults = Testify.env_defaults
+  context '.env_defaults' do
+    it "should provide an env hash with some default values" do
+      defaults = Testify.env_defaults
 
-    # Just check that these keys were defined
-    defaults.keys.should include( :testify_errors, :testify_output )
+      # Just check that these keys were defined
+      defaults.keys.should include( :testify_errors, :testify_output )
 
-    # Verify the values on these ones
-    defaults[:testify_version].should == Testify.version
-    defaults[:testify_hooks].should == { :before_all => [], :after_all => [], :before_each => [], :after_each => [], :after_status => {} }
+      # Verify the values on these ones
+      defaults[:testify_version].should == Testify.version
+      defaults[:testify_hooks].should == { :before_all => [], :after_all => [], :before_each => [], :after_each => [], :after_status => {} }
+    end
   end
 
-  it "should know its version" do
-    # This is a little cheesy, since the same code is used to generate the
-    # "correct" version as in the module itself.  But, it's a dead simple
-    # method and I'm confident it works, so I'm going to let it slide.
-    #
-    version = File.read(File.join(File.dirname(__FILE__), '..', 'VERSION')).chomp.split('.').collect { |n| n.to_i }
-
-    Testify.version.should == version
+  context '.version' do
+    it "should return an array of three integers" do
+      version = Testify.version
+      version.should have(3).parts
+      version.each { |n| n.should be_an Integer }
+    end
   end
 
 end
