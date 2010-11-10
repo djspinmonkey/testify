870ee9639afa84dce23826df91ca4a0b8ffabb75
John Hyland <jhyland@damballa.com>
2010-11-10 17:44:35 -0500

    switch from rspec to SampleFramework for mock tests

---
+++ lib/framework/sample_framework.rb
@@ -0,0 +1,37 @@
+module Testify
+  module Framework
+
+    # A sample Testify framework intended to be used while testing Testify.  A
+    # "test" consists of a line in the file, which should contain the status
+    # and optionally a colon and a message.
+    #
+    # Example test file:
+    #   fail:The thing is not in the place.
+    #   success
+    #   pending:Need to implement cat napkins.
+    #   success
+    #
+    class SampleFramework < Testify::Framework::Base
+      aka :sample
+
+      # Run some tests.
+      def call (env)
+        header = "Test Header"
+        footer = "Test Footer"
+        results = []
+
+        files(env).each do |file|
+          File.open(file).each_line do |line|
+            (status, message) = line.split(':')
+            results.push TestResult.new(status.to_sym, message)
+          end
+        end
+
+        [header, footer, results]
+      end
+
+    end
+
+  end
+end
+
