858720ee0d2ebf18347a47cdb0e4f1b6bfbfea1a
John Hyland <jhyland@damballa.com>
2010-11-12 15:19:36 -0600

    add a couple more options

--- spec/test_result_spec.rb
+++ spec/test_result_spec.rb
@@ -2,8 +2,10 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 
 describe "Testify::TestResult" do
   it "should be instantializable with a hash of attributes" do
-    res = Testify::TestResult.new(:status => :error, :message => "Your test asploded.")
+    res = Testify::TestResult.new(:file => 'woo_tests.rb', :line => 42, :status => :error, :message => "Your test asploded.")
     res.status.should == :error
     res.message.should == "Your test asploded."
+    res.file.should == 'woo_tests.rb'
+    res.line.should == 42
   end
 end
