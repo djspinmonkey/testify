7daaa0f7330510992031eb947e86e9eafef9fceb
John Hyland <jhyland@damballa.com>
2011-10-19 23:34:33 -0400

    Add some rdoc (more needed)

--- lib/middleware/base.rb
+++ lib/middleware/base.rb
@@ -3,13 +3,21 @@ module Testify
     class Base
       extend Aliasable
 
+      # By default, Testify middleware is initialized with only the next app on
+      # the stack.  If you override this, you may also need to override
+      # Runner::Base.construct_app_stack (if you are using that class).
+      #
       def initialize(app)
         @app = app
       end
 
+      # By default, the middleware base class just calls the next app on the
+      # stack - you will almost certainly want to override this method.
+      #
       def call(env)
         @app.call(env)
       end
+
     end
   end
 end
