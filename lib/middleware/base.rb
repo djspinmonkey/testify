e4073765d58b09f076ce53e02479251047a33efb
John Hyland <jhyland@damballa.com>
2011-10-19 23:31:12 -0400

    add trivial default initializer and call methods

--- lib/middleware/base.rb
+++ lib/middleware/base.rb
@@ -2,6 +2,14 @@ module Testify
   module Middleware
     class Base
       extend Aliasable
+
+      def initialize(app)
+        @app = app
+      end
+
+      def call(env)
+        @app.call(env)
+      end
     end
   end
 end
